1ea11802651ab1c6961f66eed20140f910d92cdc
jordan sankare <jordan.sankare@gmail.com>
2023-06-13 10:03:08 +0200

    removed unused var

--- src/components/Skill/index.jsx
+++ src/components/Skill/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import colors from '../../colors'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFileCircleQuestion } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faFileCircleQuestion } from "@fortawesome/free-solid-svg-icons";
 
 const Container = styled.div`
     position: relative;
