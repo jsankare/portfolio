6e032bfa30ba7a270c94965b20a46c4d6430a9fa
jordan sankare <jordan.sankare@gmail.com>
2023-06-16 15:46:22 +0200

    revert changes

--- src/router.jsx
+++ src/router.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Missdirection from './pages/404';
 import Closeup from "./pages/Closeup";
@@ -7,15 +7,13 @@ import Contact from './pages/Contact'
 
 function AppRouter() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/closeup/:id" element={<Closeup />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/404" element={<Missdirection />} />
-        <Route path="*" element={<Navigate to="/404" />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/closeup/:id" element={<Closeup />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/404" element={<Missdirection />} />
+      <Route path="*" element={<Navigate to="/404" />} />
+    </Routes>
   );
 }
 
