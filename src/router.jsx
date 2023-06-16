5103ef5c8da079bb72f567224ad125d6025161b0
jordan sankare <jordan.sankare@gmail.com>
2023-06-16 15:30:58 +0200

    change router to index 404 better

--- src/router.jsx
+++ src/router.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Missdirection from './pages/404';
 import Closeup from "./pages/Closeup";
@@ -7,13 +7,15 @@ import Contact from './pages/Contact'
 
 function AppRouter() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/closeup/:id" element={<Closeup />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/404" element={<Missdirection />} />
-      <Route path="*" element={<Navigate to="/404" />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/closeup/:id" element={<Closeup />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/404" element={<Missdirection />} />
+        <Route path="*" element={<Navigate to="/404" />} />
+      </Routes>
+    </Router>
   );
 }
 
