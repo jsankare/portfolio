80ba0510a50e43b4ed182eaf2e743658a44278e7
jordan sankare <jordan.sankare@gmail.com>
2023-09-26 15:45:40 +0200

    add pros and cons of each project with conditionnal rendering and responsive

--- src/pages/Closeup/index.jsx
+++ src/pages/Closeup/index.jsx
@@ -64,6 +64,19 @@ const TagList = styled.ul`
   padding: 0;
   margin: 0;
   gap: 10px;
+  @media (max-width: 768px) {
+    flex-wrap: wrap;
+  }
+`;
+
+const UpsandDownsList = styled.ul`
+  list-style-type: disc;
+  padding-left: 20px;
+`;
+
+const UpsandDownsItem = styled.li`
+  font-size: 16px;
+  margin-bottom: 10px;
 `;
 
 const SliderImage = styled.img`
@@ -106,81 +119,133 @@ const StyledCarousel = styled(Carousel)`
 `
 
 const TagIcon = styled.img`
-transition: 0.2s;
-  margin-bottom: 5px;
-  padding: 5px;
-  border-radius: 5px;
-  color: ${colors.secondary};
-  width: 100%;
-  height: 100%;
-  max-width: 35px;
-  max-height: 35px;
-  &:hover{
-    transform: scale(1.1);
-    }
+    transition: 0.2s;
+    margin-bottom: 5px;
+    padding: 5px;
+    border-radius: 5px;
+    color: ${colors.secondary};
+    width: 100%;
+    height: 100%;
+    max-width: 35px;
+    max-height: 35px;
+    &:hover{
+        transform: scale(1.1);
+        }
 `;
 
 const Here = styled.a`
-  text-decoration: none;
-  color: ${colors.secondary_variant};
+    text-decoration: none;
+    color: ${colors.secondary_variant};
 `
 
-// const Upsides = styled.div``
+const Upsides = styled.div``
 
-// const UpsidesTitle = styled.h4``
+const UpsidesTitle = styled.h4``
 
-// const Downsides = styled.div``
+const Downsides = styled.div``
 
-// const DownsidesTitle = styled.h4``
+const DownsidesTitle = styled.h4``
+
+const UpsAndDownsDesktop = styled.section`
+    @media (max-width: 768px) {
+        display: none;
+    }
+`
+
+const UpsAndDownsMobile = styled.section`
+    display: none;
+    text-align: left;
+    @media (max-width: 768px) {
+        display: inline;
+    }
+`
 
 const Closeup = () => {
 
-  const { id } = useParams();
-  const project = data.find((creation) => creation.title === id);
+    const { id } = useParams();
+    const project = data.find((creation) => creation.title === id);
 
-  if (!project) {
-    return <Navigate to="/404"/>
-  }
+    if (!project) {
+        return <Navigate to="/404"/>
+    }
 
-  return (
-    <Container>
-      <Helmet>
-        <title>JSankare - Projet</title>
-        <meta name="description" content="Découvrez en détail ce projet dans mon portfolio en ligne. En tant que développeur web frontend, j'ai conçu et développé cette expérience numérique en utilisant des technologies modernes pour créer une interface utilisateur intuitive et engageante." />
-      </Helmet>
-      <StyledCarousel showThumbs={false}>
-        {project.figures.map((figure, index) => (
-          <div key={index}>
-            <SliderImage src={figure} alt={`Figure ${index}`} />
-          </div>
-        ))}
-      </StyledCarousel>
-      <Wrapper>
-        <Left>
-          <Title>{project.title}</Title>
-          <TagList>
-          {project.tags.map((tag) => (
-            <TagIcon key={tag.url} src={tag.url} title={tag.name} />
-          ))}
-        </TagList>
-        {/* <Upsides>
-
-        </Upsides>
-        <Downsides>
-          
-        </Downsides> */}
-        </Left>
-        <Right>
-          <Description>{project.description}</Description>
-          {project.link && (
-            <Description>
-              Vous pouvez voir le projet <Here href={project.link} target="blank">en cliquant ici.</Here>
-            </Description>
-          )}
-        </Right>
-      </Wrapper>
-    </Container>
-  );
+    return (
+        <Container>
+        <Helmet>
+            <title>JSankare - Projet</title>
+            <meta name="description" content="Découvrez en détail ce projet dans mon portfolio en ligne. En tant que développeur web frontend, j'ai conçu et développé cette expérience numérique en utilisant des technologies modernes pour créer une interface utilisateur intuitive et engageante." />
+        </Helmet>
+        <StyledCarousel showThumbs={false}>
+            {project.figures.map((figure, index) => (
+            <div key={index}>
+                <SliderImage src={figure} alt={`Figure ${index}`} />
+            </div>
+            ))}
+        </StyledCarousel>
+        <Wrapper>
+            <Left>
+                <Title>{project.title}</Title>
+                <TagList>
+                {project.tags.map((tag) => (
+                    <TagIcon key={tag.url} src={tag.url} title={tag.name} />
+                ))}
+                </TagList>
+                <UpsAndDownsDesktop>
+                    {project.upsides && project.upsides.length > 0 && (
+                        <Upsides>
+                            <UpsidesTitle>Les points positifs :</UpsidesTitle>
+                            <UpsandDownsList>
+                                {project.upsides.map((upside, index) => (
+                                    <UpsandDownsItem key={index}>{upside}</UpsandDownsItem>
+                                ))}
+                            </UpsandDownsList>
+                        </Upsides>
+                    )}
+                    {project.downsides && project.downsides.length > 0 && (
+                        <Downsides>
+                            <DownsidesTitle>Problèmes surmontés :</DownsidesTitle>
+                            <UpsandDownsList>
+                                {project.downsides.map((downside, index) => (
+                                    <UpsandDownsItem key={index}>{downside}</UpsandDownsItem>
+                                ))}
+                            </UpsandDownsList>
+                        </Downsides>
+                    )}
+                </UpsAndDownsDesktop>
+            </Left>
+            <Right>
+                <Description>{project.description}</Description>
+                {project.link && (
+                    <Description>
+                    Vous pouvez voir le projet <Here href={project.link} target="blank">en cliquant ici.</Here>
+                    </Description>
+                )}
+                <UpsAndDownsMobile>
+                {project.upsides && project.upsides.length > 0 && (
+                        <Upsides>
+                            <UpsidesTitle>Les points positifs :</UpsidesTitle>
+                            <UpsandDownsList>
+                                {project.upsides.map((upside, index) => (
+                                    <UpsandDownsItem key={index}>{upside}</UpsandDownsItem>
+                                ))}
+                            </UpsandDownsList>
+                        </Upsides>
+                    )}
+                    {project.downsides && project.downsides.length > 0 && (
+                        <Downsides>
+                            <DownsidesTitle>Problèmes surmontés :</DownsidesTitle>
+                            <UpsandDownsList>
+                                {project.downsides.map((downside, index) => (
+                                    <UpsandDownsItem key={index}>{downside}</UpsandDownsItem>
+                                ))}
+                            </UpsandDownsList>
+                        </Downsides>
+                    )}
+                </UpsAndDownsMobile>
+            </Right>
+        </Wrapper>
+        </Container>
+    );
 };
 
 export default Closeup;
